e9bab84a9b3428fba7d3835afa5f4fc85b9206ad
manojbaniya444 <86000102+manojbaniya444@users.noreply.github.com>
2024-04-19 22:01:36 +0545

    Running app in cluster mode

---
+++ src/lib/cluster.js
@@ -0,0 +1,10 @@
+const cluster = require("node:cluster");
+
+if (cluster.isPrimary) {
+  const coresCount = require("node:os").availableParallelism();
+  for (let i = 0; i < coresCount; i++) {
+    cluster.fork();
+  }
+} else {
+  require("../index.js");
+}
